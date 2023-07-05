9cdf45806af21abd2995287ef19d6c6903869015
depp57 <deppdu5758@gmail.com>
2023-07-05 22:12:26 +0200

    chore(frontend): angular use standalone components + changeDetectionStrategy.OnPush

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   selector: 'freshbox-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [NxWelcomeComponent],
 })
 export class AppComponent {}
