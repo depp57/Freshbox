12fa1b1bf84d912804679ce3cae42ecb0d69bf29
depp57 <deppdu5758@gmail.com>
2023-08-13 16:53:03 +0200

    feat(workspace): implement SSL in all services for enhanced security
    Remember that for localhost testing, browsers may show security warnings due to the self-signed certificate. The production SSL certificates should replace these for deployment.

--- apps/backend/payment/src/main.ts
+++ apps/backend/payment/src/main.ts
@@ -4,6 +4,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { readFileSync } from 'fs';
 
 const setupSwagger = (app: INestApplication) => {
   const swaggerConfig = new DocumentBuilder()
@@ -22,17 +23,21 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
     logger: ['log', 'warn', 'error'],
+    httpsOptions: {
+      cert: readFileSync(process['env']['SSL_CERT_PATH']),
+      key: readFileSync(process['env']['SSL_KEY_PATH']),
+    },
   });
 
-  app.disable('x-powered-by'); // don't expose the backend server for security reason
+  app.disable('x-powered-by'); // Preventing exposure of used technology by disabling 'X-Powered-By' header
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   setupSwagger(app);
 
-  const port = process.env.PORT || 3002;
+  const port = process['env']['PORT'] || 3002;
   await app.listen(port);
 
-  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
+  Logger.log(`🚀 Application is running on: https://localhost:${port}`);
 }
 
 bootstrap();
