bdb5283842ad80ac8e23614d7cf5dc33463b3dcd
depp57 <deppdu5758@gmail.com>
2023-08-15 10:31:41 +0200

    feat(frontend): add pwa capabilities & improve SEO

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -5,8 +5,9 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
 import { appRoutes } from './app/pages/routes';
 import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
-import { APP_INITIALIZER, importProvidersFrom } from '@angular/core';
+import { APP_INITIALIZER, importProvidersFrom, isDevMode } from '@angular/core';
 import { env } from '@env/environment.development';
+import { provideServiceWorker } from '@angular/service-worker';
 
 function initKeycloak(keycloak: KeycloakService): () => Promise<boolean> {
   return () =>
@@ -30,10 +31,14 @@ bootstrapApplication(AppComponent, {
     provideRouter(appRoutes, withPreloading(PreloadAllModules)),
     importProvidersFrom(KeycloakAngularModule),
     {
-      provide: APP_INITIALIZER,
-      useFactory: initKeycloak,
-      multi: true,
-      deps: [KeycloakService],
+        provide: APP_INITIALIZER,
+        useFactory: initKeycloak,
+        multi: true,
+        deps: [KeycloakService],
     },
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 }).catch((error) => console.error(error));
